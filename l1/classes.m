93cdd805febf2c054f6dcf13d89016ceb83f3dd1
Iain Peet <iain@ipeet.org>
2012-02-02 10:22:43 -0500

    Copy class statistics in

---
+++ l1/classes.m
@@ -0,0 +1,17 @@
+% Records class statistics used throughout the lab
+u_A = [5; 10];
+S_A = [8 0; 0 4];
+n_A = 200;
+u_B = [10; 15];
+S_B = [8 0; 0 4];
+n_B = 200;
+
+u_C = [5; 10];
+S_C = [8 4; 4 40];
+n_C = 100;
+u_D = [15; 10];
+S_D = [8 0; 0 8];
+n_D = 200;
+u_E = [10; 15];
+S_E = [10 -5; -5 20];
+n_E = 150;
