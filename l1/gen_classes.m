23affd5c9808c0d0d42bf624b82e7a33494d0225
Iain Peet <iain@ipeet.org>
2012-02-02 13:45:12 -0500

    Whoops!

---
+++ l1/gen_classes.m
@@ -0,0 +1,8 @@
+% Generates data for all classes
+classes
+[x_a, t_a] = gen_class(u_A, S_A, n_A);
+[x_b, t_b] = gen_class(u_B, S_B, n_B);
+[x_c, t_c] = gen_class(u_C, S_C, n_C);
+[x_d, t_d] = gen_class(u_D, S_D, n_D);
+[x_e, t_e] = gen_class(u_E, S_E, n_E);
+
